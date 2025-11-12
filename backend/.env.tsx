6ad1f08316e4668b35ab6b31d860f74f8072e07f
Shyla <ShylaMK@aifnn.com>
2025-11-12 16:32:35 +0530

    Fix API URL and confirm /api/sendMail deployment

--- backend/.env.tsx
+++ backend/.env.tsx
@@ -1,6 +1,6 @@
 SMTP_USER=mk_shyla@yahoo.com
 SMTP_PASS=sxnhuqwxsxznbbgk
-VITE_API_URL=https://git-repository-aifnn.vercel.app
+VITE_API_URL=https://www.aifnn.com
 //TENANT_ID=f761f7b4-719d-4891-9ce4-a85e4656f5a3
 //CLIENT_ID=9b29ce7f-be59-4032-8e6b-5f2711a6026b
 //CLIENT_SECRET=4ced4f1c-c938-469a-9d33-bd000460063e
